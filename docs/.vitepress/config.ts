70924d5e798f95c8307e24ec68669bcc9d13d8f4
Sourav Bandyopadhyay <souravb.1998@gmail.com>
2024-06-22 21:11:49 +0530

    updated config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,36 +1,36 @@
-import { defineConfig } from 'vitepress';
+import { defineConfig } from "vitepress";
 
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
-  lang: 'en-US',
+  lang: "en-US",
   base: "/",
-  title: 'Design Patterns in Vanilla JS',
-  description: 'A comprehensive guide to design patterns in vanilla JavaScript',
+  title: "Design Patterns in Vanilla JS",
+  description: "Guide to Design Patterns in Vanilla JavaScript",
 
   themeConfig: {
     search: {
-      provider: 'local',
+      provider: "local",
     },
     nav: [
-      { text: 'Home', link: '/' },
+      { text: "Home", link: "/" },
       {
-        text: 'Dropdown Menu',
+        text: "Dropdown Menu",
         items: [
-          { text: 'Item A', link: '/item-1' },
-          { text: 'Item B', link: '/item-2' },
-          { text: 'Item C', link: '/item-3' },
+          { text: "Item A", link: "/item-1" },
+          { text: "Item B", link: "/item-2" },
+          { text: "Item C", link: "/item-3" },
         ],
       },
     ],
 
     sidebar: [
       {
-        text: 'Design Patterns',
+        text: "Design Patterns",
         items: [
-          { text: 'Command Pattern', link: '/patterns/command-pattern' },
-          { text: 'Factory Pattern', link: '/patterns/factory-pattern' },
-          { text: 'Flyweight Pattern', link: '/patterns/flyweight-pattern' },
-          { text: 'Middleware Pattern', link: '/patterns/middleware-pattern' },
+          { text: "Command Pattern", link: "/patterns/command-pattern" },
+          { text: "Factory Pattern", link: "/patterns/factory-pattern" },
+          { text: "Flyweight Pattern", link: "/patterns/flyweight-pattern" },
+          { text: "Middleware Pattern", link: "/patterns/middleware-pattern" },
         ],
       },
     ],
